7e1ac630506d24e37efea1013597c270a974a4cf
david lizhong huang <davidlizhonghuang@users.noreply.github.com>
2025-05-01 21:59:28 +1000

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 from openai import OpenAI
-api_key="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"
+api_key='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'
 #client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 client = OpenAI(api_key)
